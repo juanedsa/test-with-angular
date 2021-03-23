34a3d6be20beb2c6c3edd4ba046bf45e8e5bae83
Juan Salazar <juan1.salazar@scotiabankcolpatria.com>
2021-03-23 14:43:38 -0500

    chore: update angular 11

--- src/app/05-components-basic/components-basic.component.spec.ts
+++ src/app/05-components-basic/components-basic.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ComponentsBasicComponent } from './components-basic.component';
 
@@ -6,7 +6,7 @@ describe('ComponentsBasicComponent', () => {
   let component: ComponentsBasicComponent;
   let fixture: ComponentFixture<ComponentsBasicComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ComponentsBasicComponent]
     }).compileComponents();
